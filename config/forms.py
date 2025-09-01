4e6e15f2cc5f5e20799adbf078afe24955af98ee
Juan Garcia <juan.garcia16@utp.edu.co>
2025-08-31 20:11:12 -0500

    Validar usuario existente, se agrega el metodo el la clas Registro y se maneja el error en el html

--- config/forms.py
+++ config/forms.py
@@ -1,18 +1,27 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class Regristo(forms.Form):
     username = forms.CharField(required=True, min_length=5, max_length=40, 
     widget=forms.TextInput(attrs={ 
         'class':'form-control',
         'placeholder': 'Username',
-        })) #aplicando clases de bootstrap como si fuera html por medio de diccionarios
+    })) #aplicando clases de bootstrap como si fuera html por medio de diccionarios
     email = forms.EmailField(required=True,
     widget= forms.EmailInput(attrs={
         'class':'form-control',
         'placeholder': 'example@gmail.com'
-        }))
+    }))
     password = forms.CharField(required=True,
     widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'placeholder': 'Password'
-        }))
+    }))
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('El usuario ya existe')
+        
+        return username
