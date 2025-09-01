195ac54da519ea67786516bda623fa91e7b7dc97
Juan Garcia <juan.garcia16@utp.edu.co>
2025-08-31 19:20:33 -0500

    Estilos en el formulario de registros desde la clase en forms.py por medio de widget y diccionarios como atributos

--- config/forms.py
+++ config/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 
 class Regristo(forms.Form):
-    username = forms.CharField(required=True, min_length=5, max_length=40)
-    email = forms.EmailField(required=True)
-    password = forms.CharField(required=True)
+    username = forms.CharField(required=True, min_length=5, max_length=40, 
+    widget=forms.TextInput(attrs={ 
+        'class':'form-control',
+        'placeholder': 'Username',
+        })) #aplicando clases de bootstrap como si fuera html por medio de diccionarios
+    email = forms.EmailField(required=True,
+    widget= forms.EmailInput(attrs={
+        'class':'form-control',
+        'placeholder': 'example@gmail.com'
+        }))
+    password = forms.CharField(required=True,
+    widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'
+        }))
